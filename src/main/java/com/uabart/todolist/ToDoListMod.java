5a99691da8afe2fbd0de40e85a1ac486fb63985f
Artur Bondarenko <ua.bondarenko.artur@gmail.com>
2017-09-09 12:41:45 +0300

    version bumped to 1.0.4

--- src/main/java/com/uabart/todolist/ToDoListMod.java
+++ src/main/java/com/uabart/todolist/ToDoListMod.java
@@ -32,7 +32,7 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 @Mod(modid = ToDoListMod.MODID, version = ToDoListMod.VERSION)
 public class ToDoListMod {
     public static final String MODID = "todolist";
-    public static final String VERSION = "1.0.3";
+    public static final String VERSION = "1.0.4";
 
     @Mod.Instance
     public static ToDoListMod instance;
