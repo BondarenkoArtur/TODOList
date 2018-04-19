cab705f6355e359c7983bf5c08dce6daecdc466c
Vladislav Ivanov <deforceczt@gmail.com>
2018-04-19 13:16:47 +0300

    Bump Version

--- src/main/java/com/uabart/todolist/ToDoListMod.java
+++ src/main/java/com/uabart/todolist/ToDoListMod.java
@@ -27,7 +27,7 @@ import cpw.mods.fml.common.Mod;
 @Mod(modid = ToDoListMod.MODID, version = ToDoListMod.VERSION)
 public class ToDoListMod {
     public static final String MODID = "todolist";
-    public static final String VERSION = "1.0.6";
+    public static final String VERSION = "1.0.7";
 
     @Mod.Instance
     public static ToDoListMod instance;
