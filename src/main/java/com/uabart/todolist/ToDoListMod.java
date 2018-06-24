8f56210a9723fefc758e7268912a013503e2110b
Vladislav Ivanov <deforceczt@gmail.com>
2018-06-25 00:38:38 +0300

    Release 1.0.10

--- src/main/java/com/uabart/todolist/ToDoListMod.java
+++ src/main/java/com/uabart/todolist/ToDoListMod.java
@@ -36,7 +36,7 @@ import cpw.mods.fml.common.Mod;
 @Mod(modid = ToDoListMod.MODID, version = ToDoListMod.VERSION)
 public class ToDoListMod {
     public static final String MODID = "todolist";
-    public static final String VERSION = "1.0.9";
+    public static final String VERSION = "1.0.10";
 
     @Mod.Instance("todolist")
     public static ToDoListMod instance;
