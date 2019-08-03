39bf6ab234038ee145617b8904c32d692a59bfd6
Artur Bondarenko <ua.bondarenko.artur@gmail.com>
2019-08-03 14:48:51 +0300

    version bumped to 1.0.14

--- src/main/java/com/uabart/todolist/ToDoListMod.java
+++ src/main/java/com/uabart/todolist/ToDoListMod.java
@@ -36,7 +36,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class ToDoListMod {
 
     public static final String MODID = "todolist";
-    public static final String VERSION = "1.0.13";
+    public static final String VERSION = "1.0.14";
 
     @Mod.Instance("todolist")
     public static ToDoListMod instance;
